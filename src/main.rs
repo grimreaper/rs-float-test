52848fcfd1175cd9ec9da3e61a573d422ed9a989
Eitan Adler <lists@eitanadler.com>
2023-06-12 15:10:36 -0700

    rs-float-test: get init float basics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::rand;
-use std::rand::Rng;
-use rand_distr::{Distribution, Normal, NormalError};
-use rand_distr::uniform;
+#[warn(unused_imports)]
+
+use rand::Rng;
+// use rand::distributions::Uniform;
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let range = Uniform::new(0, 10.0);
-    let vals: Vec<float64> = (0..100).map(|_| rng.sample(&range)).collect();
+    // let range = Uniform::new(0, 10.0);
+    // range.
+    // let values: Vec<f64> = (0..100).map(|_| rng.sample(&range)).collect();
+    let values = [(); 100].map(|_| rng.gen_range(0.0..1.0));
+    // let values: Vec<f64> = rand::thread_rng().sample_iter(&range).take(100).collect();
 
-    println!(vals);
+    println!("{:?}", values);
 }
